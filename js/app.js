e05b23ab058542c7a703bba448bb3dd09ed84ef3
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-03 15:55:29 -0300

    Peggando funcao e variavel

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//Pegando funcoes e vairavel
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = document.getElementById('qtd');
+
+    alert(tipo.value);
+    alert(qtd.value);
+}
